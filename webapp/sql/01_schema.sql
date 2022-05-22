2a55131c619d66077d78006488e24b6d7739df09
Sho Kusano <rosylilly@aduca.org>
2022-05-22 16:51:28 +0900

    Change id column length

--- webapp/sql/01_schema.sql
+++ webapp/sql/01_schema.sql
@@ -2,7 +2,7 @@ USE `isucon2021_prior`;
 
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
-  `id`         VARCHAR(255) PRIMARY KEY NOT NULL,
+  `id`         VARCHAR(26) PRIMARY KEY NOT NULL,
   `email`      VARCHAR(255) NOT NULL DEFAULT '',
   `nickname`   VARCHAR(120) NOT NULL DEFAULT '',
   `staff`      BOOLEAN NOT NULL DEFAULT false,
@@ -11,7 +11,7 @@ CREATE TABLE `users` (
 
 DROP TABLE IF EXISTS `schedules`;
 CREATE TABLE `schedules` (
-  `id`         VARCHAR(255) PRIMARY KEY NOT NULL,
+  `id`         VARCHAR(26) PRIMARY KEY NOT NULL,
   `title`      VARCHAR(255) NOT NULL DEFAULT '',
   `capacity`   INT UNSIGNED NOT NULL DEFAULT 0,
   `created_at` DATETIME(6) NOT NULL,
@@ -20,9 +20,9 @@ CREATE TABLE `schedules` (
 
 DROP TABLE IF EXISTS `reservations`;
 CREATE TABLE `reservations` (
-  `id`          VARCHAR(255) PRIMARY KEY NOT NULL,
-  `schedule_id` VARCHAR(255) NOT NULL,
-  `user_id`     VARCHAR(255) NOT NULL,
+  `id`          VARCHAR(26) PRIMARY KEY NOT NULL,
+  `schedule_id` VARCHAR(26) NOT NULL,
+  `user_id`     VARCHAR(26) NOT NULL,
   `created_at`  DATETIME(6) NOT NULL,
   UNIQUE `idx_pair` (`schedule_id`, `user_id`)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
